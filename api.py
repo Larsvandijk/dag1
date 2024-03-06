4bd1699c6dd780632e9cc8ed59e1c4c1778f946a
Larsvandijk <larsvandijk1999@gmail.com>
2024-03-06 16:29:05 +0100

    2e commit

--- api.py
+++ api.py
@@ -3,6 +3,6 @@ print("Start api read application")
 import requests
 
 paginaresults = requests.get("https://catfact.ninja/facts")
-
 feitjes = paginaresults.json()
 print(feitjes['data'][0]['fact'])
+#Dit is een aanpassing
